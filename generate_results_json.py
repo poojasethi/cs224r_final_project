27d2ebd76f3e67cf52d00f006da44af2a739b61e
Ubuntu <ubuntu@ip-172-31-16-43.us-west-2.compute.internal>
2025-06-08 23:55:25 +0000

    Add new SFT model results

--- generate_results_json.py
+++ generate_results_json.py
@@ -11,11 +11,14 @@ import pandas as pd
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Don't change this.
+INPUT_JSON_PATH = "evaluation/input/ultrafeedback_heldout_prompt.json"
+
+# Update below to use the right model.
 # CHECKPOINT_PATH = "./checkpoints/dpo_model_25-06-07-102513/checkpoint-40000/" 
 # CHECKPOINT_PATH = "./checkpoints/sft_model_original/checkpoint-100000/"
-CHECKPOINT_PATH = "./checkpoints/sft_model_25-06-08-005552/checkpoint-60000/"
-INPUT_JSON_PATH = "evaluation/input/ultrafeedback_heldout_prompt.json"
-OUTPUT_JSON_PATH = "evaluation/output/sft/ultrafeedback_heldout_prompts_sft_model_original_1000000.json"
+CHECKPOINT_PATH = "./checkpoints/sft_model_25-06-08-201600/checkpoint-35000/"
+OUTPUT_JSON_PATH = "evaluation/output/sft/ultrafeedback_heldout_prompts_sft_model_25-06-08-201600_35000.json"
 
 def load_tokenizer_and_model(
     checkpoint_path = CHECKPOINT_PATH     
@@ -49,7 +52,7 @@ def generate_from_checkpoint(
     model: AutoModelForCausalLM,
     prompt: str,
     max_length: int = 512,
-    max_new_tokens: int = 1024,
+    max_new_tokens: int = 512,
     temperature: float = 0.3,
     top_p: float = 0.9,
     top_k: int = 20,
